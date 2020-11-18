4052e021f0b9ac213c2ff7ba44d40938cc3dbc9a
CarlosETurcios <turcios50@gmail.com>
2020-11-17 20:12:32 -0800

    started grapshs 3

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,10 @@
+import random
+
+
 class User:
     def __init__(self, name):
         self.name = name
+
 
 class SocialGraph:
     def __init__(self):
@@ -28,6 +32,11 @@ class SocialGraph:
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
 
+    def fisher_yates_shuffle(self, l):
+        for i in range(0, len(l)):
+            random_index = random.randint(i, len(l) - 1)
+            l[random_index], l[i] = l[i], l[random_index]
+
     def populate_graph(self, num_users, avg_friendships):
         """
         Takes a number of users and an average number of friendships
@@ -45,8 +54,28 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        for user in range(num_users):
+            self.add_user(user)
 
         # Create friendships
+        # if 1 is a frind of 2, and 2 is a friend of 1, count this as 2 friendships
+        total_friendships = avg_friendships * num_users
+
+        # create a list with all possible friendship combinations
+        # friendship_compos = [(0,1)]
+        friendship_combos = []
+
+        for user_id in range(1, num_users + 1):
+            # you can avoid this by only creating a friendship where user1 < user2
+            for friend_id in range(user_id + 1, num_users + 1):
+                friendship_combos.append((user_id, friend_id))
+             # shuffle the list
+                self.fisher_yates_shuffle(friendship_combos)
+             # then grab the first N elements from the list
+            friendship_to_make = friendship_combos[:total_friendships // 2]
+
+            for friendship in friendship_to_make:
+                self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
@@ -59,6 +88,7 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+
         return visited
 
 
