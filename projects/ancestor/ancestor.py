e27625a5c98bbf71eecf2b970b3047999b4257c9
CarlosETurcios <turcios50@gmail.com>
2020-11-17 19:34:44 -0800

    day two mvp

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -12,8 +12,27 @@
 # DFS
 
 
-def get_neighbors(id):
-    neighbors_list = []
+def earliest_ancestor(ancestors, node, path=None, ):
+    # Write a function that, given the dataset and the ID of an individual in the dataset,
+    #  returns their earliest known ancestor – the one at the farthest distance from the input individual.
+    # If there is more than one ancestor tied for "earliest", return the one with the lowest numeric ID.
+    #  If the input individual has no parents, the function should return -1.
+
+    visited = []
+
+    for i in getNeighbors(ancestors, node):
+        visited.append(i)
+    while len(visited) > 0:
+        node = visited.pop(0)
+        for i in getNeighbors(ancestors, node):
+            visited.append(i)
+        if len(visited) == 0:
+            return node
+
+    return -1
 
 
-def earliest_ancestor(ancestors, starting_node):
+def getNeighbors(tree, child_node):
+    parents = [node[0] for node in tree if node[1] == child_node]
+    parents.sort(reverse=True)
+    return parents
