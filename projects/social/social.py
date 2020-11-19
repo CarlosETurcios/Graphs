cc133bd458920bed9458e54a4a81570f48d1da5c
CarlosETurcios <turcios50@gmail.com>
2020-11-18 19:47:40 -0800

    mvp

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,4 +1,22 @@
 import random
+from collections import deque
+
+
+class Queue():
+    def __init__(self):
+        self.queue = deque()
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.popleft()
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
 
 
 class User:
@@ -8,9 +26,9 @@ class User:
 
 class SocialGraph:
     def __init__(self):
-        self.last_id = 0
-        self.users = {}
-        self.friendships = {}
+        self.last_id = 0  # current number of users
+        self.users = {}  # your user with their attributes
+        self.friendships = {}  # adjacenct list
 
     def add_friendship(self, user_id, friend_id):
         """
@@ -62,7 +80,7 @@ class SocialGraph:
         total_friendships = avg_friendships * num_users
 
         # create a list with all possible friendship combinations
-        # friendship_compos = [(0,1)]
+        # friendship_compos = [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]
         friendship_combos = []
 
         for user_id in range(1, num_users + 1):
@@ -70,12 +88,15 @@ class SocialGraph:
             for friend_id in range(user_id + 1, num_users + 1):
                 friendship_combos.append((user_id, friend_id))
              # shuffle the list
-                self.fisher_yates_shuffle(friendship_combos)
-             # then grab the first N elements from the list
-            friendship_to_make = friendship_combos[:total_friendships // 2]
+        self.fisher_yates_shuffle(friendship_combos)
+        # then grab the first N elements from the list
+        friendship_to_make = friendship_combos[:(total_friendships // 2)]
 
-            for friendship in friendship_to_make:
-                self.add_friendship(friendship[0], friendship[1])
+        for friendship in friendship_to_make:
+            self.add_friendship(friendship[0], friendship[1])
+
+    def getFriends(self, user_id):
+        return self.friendships[user_id]
 
     def get_all_social_paths(self, user_id):
         """
@@ -86,8 +107,36 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
-        visited = {}  # Note that this is a dictionary, not a set
+
+        # key: user_id value:path
+        # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        # understand the problem
+        # find every single user in users ones extended network
+        # for each of the user extended network what is the shortest path
+        # BFS shortes path
+        # make a queue
+        q = Queue()
+        visited = {}
+        # make sure its a array ([])
+        q.enqueue([user_id])
+
+        while q.size() > 0:
+            #  get the next person in line
+            current_path = q.dequeue()
+            current_person = current_path[-1]
+            # check if we visited them yet
+            if current_person not in visited:
+
+                # if not, mark as visited
+                visited[current_person] = current_path
+            # get thier firends (visted thier edges)
+                friends = self.getFriends(current_person)
+            # enquiue them
+                for friend in friends:
+                    friend_path = list(current_path)
+                    friend_path.append(friend)
+                    q.enqueue(friend_path)
 
         return visited
 
